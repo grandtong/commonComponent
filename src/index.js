ee4b6cbfbc13fb5997f020fede46155689a0fd69
liutong <liutong@deepinsighttech.com>
2024-07-09 13:38:42 +0800

    'commit'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-/*Tue Jul 09 2024 10:51:40 GMT+0800 (GMT+08:00)*/import ExcelCom from './components/ExcelCom';import IframeCom from './components/IframeCom';export { ExcelCom,IframeCom }; 
+/*Tue Jul 09 2024 13:38:33 GMT+0800 (GMT+08:00)*/import ExcelCom from './components/ExcelCom';import IframeCom from './components/IframeCom';export { ExcelCom,IframeCom }; 
