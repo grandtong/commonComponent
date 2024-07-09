a3275f8fb33b949b6cd27f617967c9486ca6d87e
liutong <liutong@deepinsighttech.com>
2024-07-09 13:46:19 +0800

    'commit'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-/*Tue Jul 09 2024 13:38:33 GMT+0800 (GMT+08:00)*/import ExcelCom from './components/ExcelCom';import IframeCom from './components/IframeCom';export { ExcelCom,IframeCom }; 
+/*Tue Jul 09 2024 13:46:10 GMT+0800 (GMT+08:00)*/import ExcelCom from './components/ExcelCom';import IframeCom from './components/IframeCom';export { ExcelCom,IframeCom }; 
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,12 +1,65 @@
-/**
- * 获取字符串长度，英文字符 长度1，中文字符长度2
- * @param {*} str
- */
-export const getStrFullLength = (str = '') =>
-    str.split('').reduce((pre, cur) => {
-        const charCode = cur.charCodeAt(0)
-        if (charCode >= 0 && charCode <= 128) {
-            return pre + 1
-        }
-        return pre + 2
-    }, 0)
+export function totop() {
+    //谷歌和火狐跳转新页面要滚动到最上面
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
+
+export function timeFix() {
+    const time = new Date()
+    const hour = time.getHours()
+    return hour < 9 ? '早上好' : hour <= 11 ? '上午好' : hour <= 13 ? '中午好' : hour < 20 ? '下午好' : '晚上好'
+}
+
+// aes encryption key
+export const encryptKeys = {
+    // key最少4位,否则报错
+    key: '1111',
+    iv: '1'
+}
+
+export const getQueryParameters = (options) => {
+    const url = options.url
+    const search = url.split('?')[1]
+    if (!search) {
+        return {}
+    }
+    return JSON.parse('{"' + decodeURIComponent(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}')
+}
+
+export const getBody = (options) => {
+    return options.body && JSON.parse(options.body)
+}
+
+export function scorePassword(pass) {
+    let score = 0
+    if (!pass) {
+        return score
+    }
+    // award every unique letter until 5 repetitions
+    const letters = {}
+    for (let i = 0; i < pass.length; i++) {
+        letters[pass[i]] = (letters[pass[i]] || 0) + 1
+        score += 5.0 / letters[pass[i]]
+    }
+
+    // bonus points for mixing it up
+    const variations = {
+        digits: /\d/.test(pass),
+        lower: /[a-z]/.test(pass),
+        upper: /[A-Z]/.test(pass),
+        nonWords: /\W/.test(pass)
+    }
+
+    let variationCount = 0
+    for (var check in variations) {
+        variationCount += variations[check] === true ? 1 : 0
+    }
+    score += (variationCount - 1) * 10
+
+    return parseInt(score)
+}
+
+export const firstLetterIsUpperCase = function (str) {
+    var reg = /^[A-Z][A-z0-9]*$/
+    return reg.test(str)
+}
