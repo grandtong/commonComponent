7f20da1b556870bdfc0300d6cc81a6e99c75114c
liutong <liutong@deepinsighttech.com>
2024-07-10 14:13:08 +0800

    'commit'

--- src/components/ExcelCom/index.vue
+++ src/components/ExcelCom/index.vue
@@ -1,87 +1,159 @@
 <template>
-  <a-table :columns="columns" :data-source="data">
-    <a slot="name" slot-scope="text">{{ text }}</a>
-    <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
-    <span slot="tags" slot-scope="tags">
-      <a-tag
-              v-for="tag in tags"
-              :key="tag"
-              :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
-      >
-        {{ tag.toUpperCase() }}
-      </a-tag>
-    </span>
-    <span slot="action" slot-scope="text, record">
-      <a>Invite 一 {{ record.name }}</a>
-      <a-divider type="vertical" />
-      <a>Delete</a>
-      <a-divider type="vertical" />
-      <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
-    </span>
-  </a-table>
+  <div style="width:100%;height:100%;margin:0;">
+    <!-- 这个id接收父页传进来的id，也就是动态接收-->
+    <div :id="id" style="width:100%;height:100%"></div>
+  </div>
+
 </template>
+
 <script>
-  import { Table } from 'ant-design-vue';
-  const columns = [
-    {
-      dataIndex: 'name',
-      key: 'name',
-      slots: { title: 'customTitle' },
-      scopedSlots: { customRender: 'name' },
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: 'Tags',
-      key: 'tags',
-      dataIndex: 'tags',
-      scopedSlots: { customRender: 'tags' },
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      scopedSlots: { customRender: 'action' },
-    },
-  ];
-
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-      tags: ['loser'],
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
-    },
-  ];
-
+  //使用echarts局部引入的方式（我这边的版本是5版本）
+  import * as echarts from 'echarts';
   export default {
+    props:["id","datas"],//接收父页传入值
     data() {
       return {
-        data,
-        columns,
+
       };
     },
+    computed:{
+
+    },
+    watch:{
+
+    },
+    mounted() {
+      _this= this;
+      this.getChartData();
+    },
+    methods: {
+      getChartData() {
+        console.log("echar内部");
+        console.log("父页传入的datas",this.datas);
+        this.drawChart();
+      },
+      drawChart() {
+        //初始化echarts实例
+        let myChart = echarts.init(document.getElementById(this.id));
+        // 清空图表，重新渲染图表
+        myChart.clear();
+
+        let option = {
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'cross',
+              label:{
+                color: '#ffffff'
+              },
+              crossStyle: {
+                color: '#ffffff'
+              }
+            }
+          },
+          legend: {
+            data: ['短信推送量', '站内消息量'],
+            textStyle:{
+              color: '#ffffff'//字体颜色
+            },
+          },
+          xAxis: [
+            {
+              type: 'category',
+              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+              axisPointer: {
+                type: 'shadow'
+              },
+              axisLabel: {
+                show: true,
+                textStyle: {
+                  color: '#ffffff'
+                }
+              }
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value',
+              name: '短信推送量(条)',
+              nameTextStyle:{
+                color:"#ffffff",
+              },
+              min: 0,
+              max: 250,
+              interval: 50,
+              axisLabel: {
+                formatter: '{value} ',
+                textStyle: {
+                  color: '#ffffff'
+                }
+              },
+              splitLine :{    //网格线
+                show:true, //隐藏或显示
+                lineStyle:{
+                  type:'dotted'    //设置网格线类型 dotted：虚线   solid:实线
+                },
+              }
+            },
+            {
+              type: 'value',
+              name: '站内消息量(条)',
+              nameTextStyle:{
+                color:"#ffffff",
+              },
+              min: 0,
+              max: 25,
+              interval: 5,
+              axisLabel: {
+                formatter: '{value} ',
+                textStyle: {
+                  color: '#ffffff'
+                }
+              },
+              splitLine :{    //网格线
+                show:true, //隐藏或显示
+                lineStyle:{
+                  type:'dotted'    //设置网格线类型 dotted：虚线   solid:实线
+                },
+              }
+            }
+          ],
+          series: [
+            {
+              name: '短信推送量',
+              type: 'bar',
+              tooltip: {
+                valueFormatter: function (value) {
+                  return value + ' 条';
+                }
+              },
+              data: [
+                2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3
+              ]
+            },
+            {
+              name: '站内消息量',
+              type: 'line',
+              yAxisIndex: 1,
+              tooltip: {
+                valueFormatter: function (value) {
+                  return value + ' 条';
+                }
+              },
+              data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+            }
+          ]
+        };
+
+        // 调用setOption
+        myChart.setOption(option)
+
+        //建议加上以下这一行代码
+        //不加的话,当浏览器窗口缩小的时候,样式会出现问题
+        window.addEventListener("resize", function() {
+          myChart.resize();
+        });
+      }
+    }
   };
 </script>
