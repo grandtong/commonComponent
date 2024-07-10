5cbec8481df9fbdeabf80e9b2972d8b4ddc3939d
liutong <liutong@deepinsighttech.com>
2024-07-10 15:31:51 +0800

    'commit'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
-/*
- * @Author: zouzheng
- * @Date: 2020-06-08 17:13:16
- * @LastEditors: zouzheng
- * @LastEditTime: 2020-06-08 17:18:31
- * @Description: 这是XXX组件（页面）
- */
 import Vue from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue'
